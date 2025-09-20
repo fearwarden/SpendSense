75f317117788467b3fe1284b9fda62292f977cf6
Milan Jovanovic <milan.jovanovic@thereputeo.com>
2025-09-20 13:21:36 +0200

    Add mcp

---
+++ agent/src/mcp_client.py
@@ -0,0 +1,30 @@
+from langchain_mcp_adapters.client import MultiServerMCPClient
+
+from src.env import DB_HOST, DB_USER, DB_PASSWORD, DB_PORT, DB_NAME
+
+client = MultiServerMCPClient(
+        {
+            "MariaDB_Server": {
+                "transport": "stdio",
+                "command": "uv",
+                "args": [
+                    "--directory",
+                    "/home/reputeo/Projects/mcp/src",
+                    "run",
+                    "server.py"
+                ],
+                "env": {
+                    "DB_HOST": DB_HOST,
+                    "DB_USER": DB_USER,
+                    "DB_PASSWORD": DB_PASSWORD,
+                    "DB_PORT": DB_PORT,
+                    "DB_NAME": DB_NAME,
+                    "MCP_READ_ONLY": "true",
+                    "MCP_MAX_POOL_SIZE": "10"
+                }
+            }
+        }
+    )
+
+async def get_tools():
+    return await client.get_tools()
